12835d2bd407a8c86e98e21aa94126e6cc28968f
github username <github email address>
2018-08-19 21:36:13 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,11 @@
 module ArtistsHelper
   def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+
+    if song.artist.name != ""
+      link_to song.artist.name, song.artist
+    else
+      link_to 'Add Artist', edit_song_path(song)
+    end
   end
+
 end
